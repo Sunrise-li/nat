91d5cadd4bca9ca576217472a8a64aa41c6dc55f
ldz <sunrise_di@163.com>
2020-04-23 15:43:33 +0800

    1

--- epoll_server/nat_server.py
+++ epoll_server/nat_server.py
@@ -1,41 +1,72 @@
 #!/usr/bin/python3
 
 import select
+
 import socket
 import queue
+import traceback
+
 from concurrent.futures import ThreadPoolExecutor
 
+max_len = 0xffffff
 
-class NATServer:
-    def tcp_forword(self,nat_server,new_sock):
-        print('tcp_forword....')
-        server = nat_server
-        client = new_sock
-        server.setblocking(False)
-        client.setblocking(False)
-        read_list = [server,client]
-        activity = True
+nat_serv_socks = queue.Queue(10)
+
+nat_serv_sock_port = 80
+
+nat_sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+#nat_sock.setsockopt(socket.SOL_SOCKET,socket.SO_KEEPALIVE,1)
+#mysql
+#nat_sock.connect(('120.53.22.183',nat_serv_sock_port))
+
+nat_sock.connect(('104.193.88.123',nat_serv_sock_port))
+
+#nat_sock.listen()
+# while self.nat_serv_socks.qsize() < 10:
+#     new_sock,addr = nat_sock.accept()
+nat_serv_socks.put(nat_sock)
+
+def tcp_forword(nat_server,new_sock):
+    print('tcp_forword....')
+    server = nat_server
+    client = new_sock
+    server.setblocking(False)
+    client.setblocking(False)
+    read_list = [server,client]
+    activity = True
+    print(client)
+    print(server)
+    try:
         while activity:
-            rs,ws,es = select,select(read_list,[],[])
+            rs,ws,es = select.select(read_list,[],[],300)
+            if not rs and not ws and not es:
+                activity = False
+                client.close()
             for r in rs:
                 if r is client:
-                    data = r.recv(self.max_len)
-                    server.sendAll(data)
+                    data = r.recv(max_len)
+                    print('client recv {0}'.format(data))
+                    server.sendall(data)
                     if not data:
                         activity = False
                 elif r is server:
-                    data = r.recv(self.max_len)
-                    client.sendAll(data)
+                    data = r.recv(max_len)
+                    print('server recv {0}'.format(data))
+                    client.sendall(data)
                     if not data:
                         activity = False
-        self.nat_serv_socks.put(server)
-        return True
+    except Exception as e:
+        traceback.print_exc()
+    print('连接关闭。。。')
+    nat_serv_socks.put(server)
+    return True
 
+class NATServer:
     def __init__(self):
         self.init_thread_pool()
         self.init_server_sock()
-        self.nat_serv_socks = queue.Queue(10)
-        self.init_nat_serv_sock()
+      
+        #self.init_nat_serv_sock()
         #self.forword_pool.submit(self.init_nat_serv_sock,())
         #self.init_epoll()
 
@@ -55,16 +86,6 @@ class NATServer:
         self.epoll = select.epoll()
         self.epoll.register(self.server_socket.fileno(),select.EPOLLIN)
     
-    def init_nat_serv_sock(self):
-        self.nat_serv_sock_port = 3306
-
-        print('nat sock 注册端口 {0}'.format(self.nat_serv_sock_port))
-        nat_sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        nat_sock.connect(('120.53.22.183',self.nat_serv_sock_port))
-        #nat_sock.listen()
-        # while self.nat_serv_socks.qsize() < 10:
-        #     new_sock,addr = nat_sock.accept()
-        self.nat_serv_socks.put(nat_sock)
 
 
     def start(self):
@@ -75,20 +96,24 @@ class NATServer:
             for r in rs:
                 try:
                     print('fid {0}  fid{1}'.format(r.fileno(),self.server_socket.fileno()))
-                    print(r.fileno() == self.server_socket.fileno())
                     if r.fileno() == self.server_socket.fileno():
                         new_client,new_addr = self.server_socket.accept()
                         print('{0} connect success.'.format(new_addr))
+                        nat_sock = nat_serv_socks.get()
                         new_sock_fd = new_client.fileno()
+                        print('client fd {0} server fd{1}'.format(new_sock_fd,nat_sock.fileno()))
                         # self.sock_conns[new_sock_fd] = new_client;
                         # self.sock_addrs[new_sock_fd] = new_addr()
-                        read_list.append(new_client)
-                    else:
-                        nat_sock = self.nat_serv_socks.get()
-                        print('开始转发数据包。。。{0} ----{1}'.format(nat_sock,r))
-                        future = self.forword_pool.submit(self,self.tcp_forword,(nat_sock,r))
-                        read_list.remove(r)
-                        print(future.result())
+                       
+                        future = self.forword_pool.submit(tcp_forword,nat_sock,new_client)
+                        # read_list.remove(r)
+                        #read_list.append(new_client)
+                    # else:
+                        
+                    #     print('开始转发数据包。。。{0} ----{1}'.format(nat_sock,r))
+                    #     future = self.forword_pool.submit(tcp_forword,(nat_sock,r))
+                    #     read_list.remove(r)
+                    #     print(future.result())
                 except Exception  as e:
                     print(e)
                    
